6df9943ca8be5f5fe9b27e56dc3e55e07f58f6c1
yannick <yayadesigner@gmail.com>
2015-05-19 15:25:53 +0200

    Premier projet WEB ASP.NET

---
+++ pam-web-01/Controllers/PamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pam.Controllers
+{
+    public class PamController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
