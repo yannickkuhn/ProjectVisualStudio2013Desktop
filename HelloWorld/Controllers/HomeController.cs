116030b408bfb4d0911579ed0f85b26842f3362f
yannick <yayadesigner@gmail.com>
2015-05-28 14:51:14 +0200

    Fin de première partie "Bases d'ASP.NET MVC" (openclassroom)

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return View("Error");
+            else
+            {
+                ViewBag.Nom = id;
+                return View();
+            }
+        }
+
+        public ActionResult ListeClients()
+        {
+            Client clients = new Client();
+            ViewData["Clients"] = clients.ObtenirListeClients();
+            return View();
+        }
+
+        public ActionResult ChercheClient(string id)
+        {
+            ViewData["Nom"] = id;
+            Client clients = new Client();
+            Client client = clients.ObtenirListeClients().FirstOrDefault(c => c.Nom == id);
+            if (client != null)
+            {
+                ViewData["Age"] = client.Age;
+                return View("Trouve");
+            }
+            return View("NonTrouve");
+        }
+	}
+}
---
+++ HelloWorld/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Client
+    {
+        public string Nom { get; set; }
+        public int Age { get; set; }
+
+        public List<Client> ObtenirListeClients()
+        {
+            return new List<Client>
+        {
+            new Client { Age = 33, Nom = "Nicolas"},
+            new Client { Age = 30, Nom = "Delphine"},
+            new Client { Age = 33, Nom = "Jérémie"},
+            new Client { Age = 1, Nom = "Timéo"}
+        };
+        }
+    }
+}
